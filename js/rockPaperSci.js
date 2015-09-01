d87d59f020637efa18c1de24e66fe8566e29329f
Barbora Doslikova <dev@barboradoslikova.com>
2015-09-01 09:49:41 -0600

    Update rockPaperSci.js

--- js/rockPaperSci.js
+++ js/rockPaperSci.js
@@ -1,25 +1,3 @@
-
-Skip to content
-This repository
-
-    Pull requests
-    Issues
-    Gist
-
-    @BarboraDoslikova
-
-1
-0
-
-    0
-
-BarboraDoslikova/games-website
-
-games-website/js/rockPaperSci.js
-@BarboraDoslikova BarboraDoslikova 18 seconds ago Update rockPaperSci.js
-
-1 contributor
-67 lines (60 sloc) 2.082 kB
 function rockPaperSci() {
     // Asks the user to make a choice.
     var userChoice = prompt("Do you choose ROCK, PAPER or SCISSORS?").toUpperCase();
@@ -86,8 +64,3 @@ function rockPaperSci() {
     // If it's a tie, ask to go to the 2nd round.
     // What if there is a 4th option, e.g. a toliet?
 }
-
-    Status API Training Shop Blog About Pricing 
-
-    © 2015 GitHub, Inc. Terms Privacy Security Contact Help 
-
